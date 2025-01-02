98011cfc720fdeb915128621caa00f87b255baa2
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-01-02 12:14:43 +0100

    Add AWS S3 services and configuration support
    Added package references to Microsoft.Extensions.Configuration.Abstractions, Binder, and Options in Moongazing.Ink.Infrastructure.csproj. Introduced InfrastructureServiceRegistration class with AddInfrastructureServices method to configure and register AWS S3 services and settings into the dependency injection container. This includes binding AwsS3Settings, registering IAmazonS3 client, TransferUtility, and custom IStorageService implementation.

---
+++ Moongazing.Ink/src/Moongazing.Ink.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,40 @@
+﻿using Amazon.S3;
+using Amazon.S3.Transfer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Moongazing.Ink.Infrastructure.Aws.Configurations;
+using Moongazing.Ink.Infrastructure.Aws.Services;
+
+namespace Moongazing.Ink.Infrastructure;
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<AwsS3Settings>(configuration.GetSection("AWS").Bind);
+        services.AddSingleton(sp => sp.GetService<IOptions<AwsS3Settings>>()!.Value);
+
+        services.AddSingleton<IAmazonS3>(sp =>
+        {
+            var awsConfig = sp.GetService<AwsS3Settings>();
+            var config = new AmazonS3Config
+            {
+                RegionEndpoint = Amazon.RegionEndpoint.GetBySystemName(awsConfig!.Region),
+                ServiceURL = awsConfig.ServiceURL,
+                ForcePathStyle = true
+            };
+            return new AmazonS3Client(awsConfig.AccessKey, awsConfig.SecretKey, config);
+        });
+
+        services.AddSingleton(sp =>
+        {
+            var s3Client = sp.GetService<IAmazonS3>();
+            return new TransferUtility(s3Client);
+        });
+
+        services.AddSingleton<IStorageService, StorageService>();
+
+        return services;
+    }
+}
