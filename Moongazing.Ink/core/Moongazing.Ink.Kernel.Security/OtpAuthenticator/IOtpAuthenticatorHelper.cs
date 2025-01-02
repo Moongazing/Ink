ad837fe14d90ce31d91d2b28ddb06589689549d3
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-01-02 11:50:01 +0100

    Ref

--- Moongazing.Ink/core/Moongazing.Ink.Kernel.Security/OtpAuthenticator/IOtpAuthenticatorHelper.cs
+++ Moongazing.Ink/core/Moongazing.Ink.Kernel.Security/OtpAuthenticator/IOtpAuthenticatorHelper.cs
@@ -4,6 +4,5 @@ public interface IOtpAuthenticatorHelper
 {
     public Task<byte[]> GenerateSecretKeyAsync();
     public Task<string> ConvertSecretKeyToStringAsync(byte[] secretKey);
- 
     public Task<bool> VerifyCodeAsync(byte[] secretKey, string code); 
 }  
