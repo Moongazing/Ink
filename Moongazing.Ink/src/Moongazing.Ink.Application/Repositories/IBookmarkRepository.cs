1a0ae65488efd0b139188e8c4f9a4c915e8b75f9
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-01-02 15:14:12 +0100

    Add BookmarkRepository and update project references
    Updated Moongazing.Ink.Application.csproj to include a project reference to Moongazing.Ink.Domain.csproj. Modified Moongazing.Ink.Persistence.csproj to remove folder inclusion for Repositories and add project references to Moongazing.Ink.Kernel.Persistence.csproj, Moongazing.Ink.Application.csproj, and Moongazing.Ink.Domain.csproj. Added IBookmarkRepository interface in Moongazing.Ink.Application.Repositories namespace, extending IAsyncRepository and IRepository for BookmarkEntity with Guid as the identifier. Added BookmarkRepository class in Moongazing.Ink.Persistence.Repositories namespace, extending EfRepositoryBase for BookmarkEntity with Guid as the identifier and implementing IBookmarkRepository.

---
+++ Moongazing.Ink/src/Moongazing.Ink.Application/Repositories/IBookmarkRepository.cs
@@ -0,0 +1,14 @@
+﻿using Moongazing.Ink.Domain.Entities;
+using Moongazing.Ink.Kernel.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moongazing.Ink.Application.Repositories
+{
+    public interface IBookmarkRepository: IAsyncRepository<BookmarkEntity, Guid>, IRepository<BookmarkEntity, Guid>
+    {
+    }
+}
---
+++ Moongazing.Ink/src/Moongazing.Ink.Persistence/Repositories/BookmarkRepository.cs
@@ -0,0 +1,19 @@
+﻿using Moongazing.Ink.Application.Repositories;
+using Moongazing.Ink.Domain.Entities;
+using Moongazing.Ink.Kernel.Persistence.Repositories;
+using Moongazing.Ink.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moongazing.Ink.Persistence.Repositories;
+
+public class BookmarkRepository : EfRepositoryBase<BookmarkEntity, Guid, BaseDbContext>,IBookmarkRepository
+{
+    public BookmarkRepository(BaseDbContext context):base(context)
+    {
+
+    }
+}
