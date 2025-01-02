e161bf9be7f2372270846a72e47fb50033f0bdc0
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-01-02 11:20:30 +0100

    Add database migration services and extensions
    Added new package references to `Moongazing.Ink.Kernel.Persistence.csproj` including `EntityFrameworkCore.Query`, `Microsoft.AspNetCore.Http.Abstractions`, `Microsoft.Identity.Client`, `System.Linq.Dynamic.Core`, and `System.Net.Http`.
    
    Introduced `ApplicationBuilderDbMigrationApplierExtensions` to extend `IApplicationBuilder` with `UseDbMigrationApplier` method.
    
    Created `DbMigrationApplierService` implementing `IDbMigrationApplierService<TDbContext>` to ensure database migrations are applied.
    
    Added `DbMigrationHelperExtensions` with `EnsureDbApplied` method for `DatabaseFacade`.
    
    Defined `IDbMigrationApplierService` and `IDbMigrationApplierService<TDbContext>` interfaces.
    
    Introduced `ServiceCollectionDbMigrationApplierExtensions` to extend `IServiceCollection` with `AddDbMigrationApplier` method for DI registration.

---
+++ Moongazing.Ink/core/Moongazing.Ink.Kernel.Persistence/MigrationApplier/IDbMigrationApplierService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Moongazing.Ink.Kernel.Persistence.MigrationApplier;
+
+public interface IDbMigrationApplierService
+{
+    void Initialize();
+}
+
+public interface IDbMigrationApplierService<TDbContext> : IDbMigrationApplierService
+    where TDbContext : DbContext
+{
+
+
+}
+
